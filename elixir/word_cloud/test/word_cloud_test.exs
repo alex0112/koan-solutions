147eb2653a6d4f8fcd7121065c86143922f6d46f
alex0112 <alarsen0112@gmail.com>
2019-05-27 21:38:06 -0600

    Tests for commas

--- elixir/word_cloud/test/word_cloud_test.exs
+++ elixir/word_cloud/test/word_cloud_test.exs
@@ -1,8 +1,17 @@
 defmodule WordCloudTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest WordCloud
 
-  test "splits on commas" do
-    assert WordCloudTest.split()
+  test "splits on spaces" do
+    assert WordCloud.split("a a")   == ["a", "a"]
+    assert WordCloud.split("a  a")  == ["a", "a"] ## Multiple spaces
+    assert WordCloud.split(" a a ") == ["a", "a"] ## Padded string
   end
+  
+  # test "splits on commas" do
+  #   assert WordCloud.split("a,a")   == ["a", "a"]
+  #   assert WordCloud.split("a,,a")  == ["a", "a"]
+  #   assert WordCloud.split(",a,a,") == ["a", "a"]
+  # end
+  
 end
