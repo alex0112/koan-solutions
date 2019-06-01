827db89b547d29f357e04c2bb983a88c06d35220
alex0112 <alarsen0112@gmail.com>
2019-05-31 21:58:51 -0600

    Additional unit tests

--- elixir/word_cloud/test/word_cloud_test.exs
+++ elixir/word_cloud/test/word_cloud_test.exs
@@ -10,4 +10,17 @@ defmodule WordCloudTest do
       assert WordCloud.split(" " <> "a" <> @char <> "a" <> " ")     == ["a", "a"] ## Padded string (whitespace)
     end
   end
+
+  describe "count" do
+    test "Properly creates a wordmap" do
+      assert WordCloud.count(~w(cottleson cottleson cottleson pie)) == %{"cottleson" => 3, "pie" => 1}
+    end
+  end
+
+  describe "integration of split and count" do
+    test "Properly creates a wordmap from a split string." do
+      assert WordCloud.split("cottleson cottleson cottleson pie") |> WordCloud.count == %{"cottleson" => 3, "pie" => 1}
+    end
+  end
+  
 end
