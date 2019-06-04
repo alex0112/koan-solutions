b3962f0e297e6d0c9435959b53c46c269d7642ee
alex0112 <alarsen0112@gmail.com>
2019-06-03 20:51:56 -0600

    Primitives, IO, and bitwise XOR

--- rust/hello/src/main.rs
+++ rust/hello/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
     println!("Hello, world!");
+    primitives();
+}
+
+
+fn primitives() {
+/*    let my_bool: bool = true;
+    let my_float: f64 = 3.14159;
+    let my_integer: u32 = 30056;
+    let mut my_mutable_integer: u32 = 0; */
+
+    // bitwise math
+    println!("The 1001 XOR 1100 is {:b}",  (0b1001 ^ 0b1100));
 }
